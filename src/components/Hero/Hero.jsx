f00cd4abbec08f67435f02c78e80145a59e0628a
Aman Verma <verma.aman0517@gmail.com>
2025-11-03 15:24:57 +0530

    Enable resume download on click
    Added functionality to download resume on click.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/Profile1.jpg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import resume from "../../assets/Aman-Verma_resume_1.pdf";
 
 const Hero = () => {
+
+     // Function to trigger resume download
+    const handleDownload = () => {
+        const link = document.createElement("a");
+        link.href = resume;
+        link.download = "Aman-Verma_resume_1.pdf"; // The file name for download
+        link.click();
+    };
+
     return (
         <div id="home" className="hero">
             <img src={profile_img} alt="Hero Image" />
@@ -11,7 +21,7 @@ const Hero = () => {
             <p>Versatile software developer skilled in building robust front-end interfaces and efficient back-end systems.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-                <div className="hero-resume">My Resume</div>
+                <div className="hero-resume" onClick={handleDownload}>My Resume</div>
             </div>
         </div>
     );
